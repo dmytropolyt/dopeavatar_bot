2c6cda53c47d69c7900ef411f62af53330c963a6
dmytropolyt <drago5490@gmail.com>
2023-06-11 02:38:33 +0300

    enb vars

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / '.env')
 
-bot_token = '6241651323:AAH1HzzPFaTRIRXEgQem5a4pb2CryK8lb5w'
-bot_user_name = 'dopeavatar_bot'
-URL = 'https://dope-avatar-bot.onrender.com/'
+bot_token = os.environ.get('BOT_TOKEN')
+bot_user_name = os.environ.get('BOT_USERNAME')
+URL = os.environ.get('URL')
